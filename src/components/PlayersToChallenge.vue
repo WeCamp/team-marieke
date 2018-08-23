7ee6fd592d1ab753862b0f19ab7a596f81596d74
Jakob Buis <jakob.buis@in10.nl>
2018-08-23 22:13:37 +0200

    show the state of the challenge

--- src/components/PlayersToChallenge.vue
+++ src/components/PlayersToChallenge.vue
@@ -2,7 +2,19 @@
     <div>
         <h1>Players to challenge</h1>
 
-        <table>
+        <div class="alert alert-waiting" v-if="state === 'waiting'">
+            Your challenge is awaiting a response
+        </div>
+
+        <div class="alert alert-accepted" v-if="state === 'accepted'">
+            Your challenge has been accepted by {{ usernameOfChallengedToDuel }}
+        </div>
+
+        <div class="alert alert-rejected" v-if="state === 'rejected'">
+            Your challenge has been rejected
+        </div>
+
+        <table v-if="state === 'initial'">
             <thead>
                 <tr>
                     <th>Name</th>
@@ -29,6 +41,8 @@
             return {
                 users: [],
                 usernameOfChallengedToDuel: null,
+                state: 'initial', // initial -> waiting -> accepted
+                                  //                    -> rejected
             };
         },
 
@@ -45,7 +59,10 @@
         methods: {
             challenge(username) {
                 this.usernameOfChallengedToDuel = username;
-            }
+                this.state = 'waiting';
+                axios.post('http://localhost:8080/challenge', {username: this.usernameOfChallengedToDuel})
+                    .then(() => this.state = 'waiting');
+            },
         },
 
         props: [
@@ -53,3 +70,23 @@
         ],
     };
 </script>
+
+<style scoped>
+.alert {
+    padding: 1em;
+    border: 1px solid black;
+    border-radius: 1em;
+}
+
+.alert-waiting {
+    background-color: #fc6;
+}
+
+.alert-accepted {
+    background-color: #cf6;
+}
+
+.alert-rejected {
+    background-color: #f99;
+}
+</style>
