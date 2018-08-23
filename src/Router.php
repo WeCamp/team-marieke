ab807d0db1a6b4ea7d0f872df08ac982681439ae
Gediminas Sedbaras <gediminas@messagebird.com>
2018-08-23 23:02:49 +0200

    assign player as attribute of request

--- src/Router.php
+++ src/Router.php
@@ -6,11 +6,15 @@ use Amp\Http\Server\Request;
 use Amp\Http\Server\Response;
 use Amp\Http\Status;
 use CorrectHorseBattery\Authentication\AuthenticationContext;
+use CorrectHorseBattery\Authentication\NoPlayerSignedOn;
+use CorrectHorseBattery\Domain\PlayerDoesNotExist;
 use CorrectHorseBattery\Repositories\Players;
 
 class Router
 {
     private $routes;
+
+    private $authenticationContextFactory;
 
     public function __construct()
     {
@@ -21,7 +25,7 @@ class Router
             }
             return $instance;
         };
-        $authenticationContext = function () use ($players): AuthenticationContext {
+        $this->authenticationContextFactory = function () use ($players): AuthenticationContext {
             static $instance = null;
             if ($instance === null) {
                 $instance = new AuthenticationContext($players());
@@ -33,14 +37,11 @@ class Router
             '/' => function () {
                 return new \CorrectHorseBattery\Controllers\Login();
             },
-            '/playerstochallenge' => function () use ($authenticationContext) {
-                return new \CorrectHorseBattery\Controllers\ChallengeablePlayers($authenticationContext());
+            '/playerstochallenge' => function () {
+                return new \CorrectHorseBattery\Controllers\ChallengeablePlayers();
             },
-            '/challengeplayer' => function () use ($authenticationContext, $players) {
-                return new \CorrectHorseBattery\Controllers\ChallengePlayer(
-                    $authenticationContext(),
-                    $players()
-                );
+            '/challengeplayer' => function () use ($players) {
+                return new \CorrectHorseBattery\Controllers\ChallengePlayer($players());
             },
         ];
     }
@@ -48,6 +49,20 @@ class Router
     public function route(Request $request)
     {
         $url = $request->getUri()->getPath();
+
+        if ($url !== '/') {
+            /** @var AuthenticationContext $authenticationContext */
+            $authenticationContext = ($this->authenticationContextFactory)();
+            try {
+                $currentSignedOnPlayer = $authenticationContext->currentSignedOnPlayer($request);
+            } catch (NoPlayerSignedOn $e) {
+                return new Response(Status::BAD_REQUEST, [], 'You must send a "Player" header with your username');
+            } catch (PlayerDoesNotExist $e) {
+                return new Response(Status::FORBIDDEN, [], 'You do not exist');
+            }
+
+            $request->setAttribute('player', $currentSignedOnPlayer);
+        }
 
         // If a controller for this URL exists, create it and execute it with the request
         if (isset($this->routes[$url])) {
