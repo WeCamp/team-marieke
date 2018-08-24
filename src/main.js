1b1118f0f7b747d0585084cfaa3b5611ac014f71
Jakob Buis <jakob.buis@in10.nl>
2018-08-24 14:30:54 +0200

    remove debug and commented code

--- src/main.js
+++ src/main.js
@@ -4,11 +4,8 @@ import axios from 'axios';
 
 window.Axios = axios;
 window.ws = new WebSocket("ws://localhost:9001/");
-window.ws.addEventListener("message", function(e) {
-  console.log(e);
-});
 
-    Vue.config.productionTip = false;
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
